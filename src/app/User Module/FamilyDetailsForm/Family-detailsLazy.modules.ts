5913f2b3632f632c69fb18db44f402ff1fc72e99
sasturash <sasturasherikar93@gmail.com>
2020-01-24 11:46:00 +0530

    done by sastura

---
+++ src/app/User Module/FamilyDetailsForm/Family-detailsLazy.modules.ts
@@ -0,0 +1,29 @@
+import { RouterModule, Routes } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { ShowHidePasswordModule } from 'ngx-show-hide-password';
+import { FamilyDetailsComponent } from './Family-details.component'
+
+const familyroute: Routes = [
+
+    // { path:'' , redirectTo:'register' , pathMatch:'full' } ,
+    { path: '', component: FamilyDetailsComponent}
+
+]
+
+
+@NgModule({
+
+    declarations: [FamilyDetailsComponent],
+    imports: [CommonModule,
+        ShowHidePasswordModule,
+        ReactiveFormsModule, RouterModule.forChild(familyroute)],
+    providers: [],
+    exports: [FamilyDetailsComponent]
+
+})
+
+export class FamilyLazyClass{
+
+}
--- src/app/route.config.ts
+++ src/app/route.config.ts
@@ -4,12 +4,14 @@ import { AstroreligionComponent } from './User Module/AstroandreligionForm/Astro
 import { ProfileDetailsComponent } from './User Module/ProfileDetailsForm/profile-details.component'
 //import {}  from './User Module/RegisterForm/RegisterLazy.module#RegisterLazyClass'
 //import {} from './User Module/AstroandreligionForm/AstroreligionLazy.module#AstroreligionLazyClass'
-//import {} from './User Module/ProfileDetailsForm/ProfileDetailsLazy.module#RegisterLazyClass'
+//import {} from './User Module/FamilyDetailsForm/Family-detailsLazy.modules'
+
 export const MyRoute: Routes = [
 
     
     { path: '', loadChildren: './User Module/RegisterForm/RegisterLazy.module#RegisterLazyClass' },
     { path:'astroandreligion' , loadChildren:'./User Module/AstroandreligionForm/AstroreligionLazy.module#AstroreligionLazyClass'},
-    { path:'profiledetails' , loadChildren:'./User Module/ProfileDetailsForm/ProfileDetailsLazy.module#RegisterLazyClass'}
+    { path:'profiledetails' , loadChildren:'./User Module/ProfileDetailsForm/ProfileDetailsLazy.module#RegisterLazyClass'},
+    { path:'familydetails' , loadChildren:'./User Module/FamilyDetailsForm/Family-detailsLazy.modules#FamilyLazyClass'}
 ]
 
