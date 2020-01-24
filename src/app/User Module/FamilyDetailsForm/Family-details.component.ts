82410be13a83053a9716c7f1fa77fbafbbfbbeef
Farukh786-Crypto <60135145+Farukh786-Crypto@users.noreply.github.com>
2020-01-24 20:08:28 +0530

    aa
    yes

--- src/app/User Module/FamilyDetailsForm/Family-details.component.ts
+++ src/app/User Module/FamilyDetailsForm/Family-details.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 // import { ToastrService } from 'ngx-toastr';
 @Component({
+<<<<<<< Updated upstream
   selector: 'app-family-details',
   templateUrl: './Family-details.component.html'
+=======
+  selector: 'familydetails_app',
+  templateUrl: './Family-details.componet.html'
+>>>>>>> Stashed changes
  
 })
 export class FamilyDetailsComponent{
