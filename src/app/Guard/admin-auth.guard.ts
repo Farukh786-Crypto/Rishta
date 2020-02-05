000fb18ca7a2621f8523615e6c5f65f71b319197
DadasahebBharat <rajubharat78@gamil.com>
2020-02-05 18:14:29 +0530

    raj added auth guard on 5-2-20

---
+++ src/app/Guard/admin-auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Router,CanActivate} from '@angular/router';
+@Injectable({
+    providedIn:'root'
+})
+export class AdminAuthGuard implements CanActivate{
+
+    constructor(private router:Router){}
+    canActivate()
+    {
+        if(localStorage.getItem('userRole')==='admin')
+        {
+            return true;
+        }
+        else
+        {
+            this.router.navigate(['./login']);
+            return false;
+        }
+
+    }
+}
