c6bf8da2625da523ef0c07099ff068ac45be7172
sasturash <sasturasherikar93@gmail.com>
2020-02-08 15:22:32 +0530

    commit
    commit by sastura

--- src/app/route.config.ts
+++ src/app/route.config.ts
@@ -5,7 +5,7 @@ import { AdminAuthGuard } from './Guard/admin-auth.guard';
 import { UsersAuthGuard } from './Guard/users-auth.guard';
 export const MyRoute: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', loadChildren: './login/login.module#LoginModule',canActivate:[AuthGuard] },
+    { path: 'login', loadChildren: './login/login.module#LoginModule'},
     { path: 'admin', loadChildren: './layout/layout.module#LayoutModule',canActivate:[AdminAuthGuard]},
     { path: 'users', loadChildren: './User Module/user.module#UserModule',canActivate:[UsersAuthGuard]},
     // { path: 'sign-up', loadChildren: './signup/signup.module#SignupModule' },
