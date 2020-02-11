a6cc935a41732fb82eae315930727ebbb78b9208
sasturash <sasturasherikar93@gmail.com>
2020-02-11 19:09:58 +0530

    commit by sastura 11022020 0709 PM
    commit

--- src/app/User Module/ProfileDetailsForm/profile-details.component.ts
+++ src/app/User Module/ProfileDetailsForm/profile-details.component.ts
@@ -8,45 +8,45 @@ import { ProfileDetailsService } from './profile-details.service';
   selector: 'app-profile-details',
   templateUrl: './profile-details.component.html',
   styleUrls: ['./profile-details.component.css'],
-  providers:[ProfileDetailsService]
+  providers: [ProfileDetailsService]
 })
-export class ProfileDetailsComponent implements OnInit{
- 
+export class ProfileDetailsComponent implements OnInit {
 
-  ProfileDetails:FormGroup
-  isSubmitted:boolean;
-  ProfileInfo:any
-  mothertounge:{};
-  allreligions:{};
-  allcastes:{};
-  allsubcastes:{};
-  
-  constructor(private spinner: NgxSpinnerService,private pd:ProfileDetailsService) { 
 
-    
+  ProfileDetails: FormGroup
+  isSubmitted: boolean;
+  ProfileInfo: any
+  mothertounge: {};
+  allreligions: {};
+  allcastes: {};
+  allsubcastes: {};
+
+  constructor(private spinner: NgxSpinnerService, private pd: ProfileDetailsService) {
+
+
     this.ProfileDetails = new FormGroup({
       Name: new FormControl('', Validators.required),
       Dob: new FormControl('', Validators.required),
       religion: new FormControl('', Validators.required),
       Mothertng: new FormControl('', Validators.required),
-      Caste: new FormControl('',Validators.required),
-      SubCaste: new FormControl('',Validators.required),
-      Manglik: new FormControl('',Validators.required),
-      Horoscope: new FormControl('',Validators.required),
+      Caste: new FormControl('', Validators.required),
+      SubCaste: new FormControl('', Validators.required),
+      Manglik: new FormControl('', Validators.required),
+      Horoscope: new FormControl('', Validators.required),
       Marrital: new FormControl('', Validators.required),
-      Height: new FormControl('',Validators.required),
-      chkbx: new FormControl('',Validators.required)
-  })
+      Height: new FormControl('', Validators.required),
+      chkbx: new FormControl('', Validators.required)
+    })
   }
   ngOnInit() {
     /** spinner starts on init */
     this.spinner.show();
     this.pd.getMothertounge().subscribe(
-      data=>this.mothertounge=data
-      );
-      this.pd.getReligion().subscribe(
-        data=>this.allreligions=data
-            );
+      data => this.mothertounge = data
+    );
+    this.pd.getReligion().subscribe(
+      data => this.allreligions = data
+    );
     setTimeout(() => {
       /** spinner ends after 5 seconds */
       this.spinner.hide();
@@ -57,49 +57,70 @@ export class ProfileDetailsComponent implements OnInit{
   // {
   //     this.pd.getMothertounge().subscribe(res=>{this.ProfileInfo=res},err=>{this.ProfileInfo=err})
   // }
-  public getrel()
-  {
+  public getrel() {
     this.pd.getReligion().subscribe(
-      data=>this.allreligions=data
-          );
+      data => this.allreligions = data
+    );
   }
-  onChangeReligion(religion_Id: number) {
-    // if (religion_Id) {
-      this.pd.getCaste(religion_Id).subscribe(
-        data => 
-          this.allcastes = data
-         // this.subcastes = null;
-        
-      );
-        }
-     
-      
-        onChangeSubCaste(caste_id: number) {
-          // if (religion_Id) {
-            this.pd.getSubcaste(caste_id).subscribe(
-              data => 
-                this.allsubcastes = data
-               // this.subcastes = null;
-              
-            );
-              }
+  // onChangeReligion(religion_Id: number) {
+  //    if (religion_Id) {
+  //     this.pd.getCaste(religion_Id).subscribe(
+  //       data => 
+  //         this.allcastes = data;
+  //         this.allsubcastes = null;}
+
+  //     );
 
 
+  onChangeReligion(religion_id: number) {
+   
+    if (religion_id) {
+      this.pd.getCaste(religion_id).subscribe(
+        data => {
+          this.allcastes = data;
+          this.allsubcastes = null;
+        });
+    }
+    else {
+      this.allcastes = null;
+      this.allsubcastes = null;
 
-  onSubmit(data:any) {
+    }
+  }
+  onChangeSubCaste(caste_id: number) {
+    if (caste_id) {
+      this.pd.getSubcaste(caste_id).subscribe(
+        data => {
+          this.allsubcastes = data;
+          //this.allsubcastes = null;
+        });
+    }
+    else {
+      this.allsubcastes = null;
+      //this.allsubcastes = null;
+  
+    }
+  }
+
+  onSubmit(data: any) {
     this.isSubmitted = true;
     if (!this.ProfileDetails.valid) {
       alert(JSON.stringify(data));
-      
-    //  this.toastr.success('Hello world!', 'Record SucessFully!');
-      
-      
+  
+      //  this.toastr.success('Hello world!', 'Record SucessFully!');
+  
+  
       return false;
     } else {
       alert(JSON.stringify(this.ProfileDetails.value))
       //this.toastr.remove;
     }
-
+  
   }
- 
 }
+
+
+
+
+
+
--- src/app/User Module/ProfileDetailsForm/profile-details.service.ts
+++ src/app/User Module/ProfileDetailsForm/profile-details.service.ts
@@ -9,28 +9,25 @@ export class ProfileDetailsService {
   constructor(private http: HttpClient) { }
 
   public getMothertounge() {
-    let url='http://192.168.0.127:8082/api/getmothertoungue'
+    let url = 'http://192.168.0.127:8082/api/getmothertoungue'
     return this.http.get(url);
 
   }
-  public getReligion()
-  {
-    let url='http://192.168.0.127:8082/api/religion/getReligion'
+  public getReligion() {
+    let url = 'http://192.168.0.127:8082/api/religion/getReligion'
     return this.http.get(url);
 
   }
-  public getCaste(religion_Id:number)
-  {
-    let url='http://192.168.0.127:8082/api/caste/getCaste?religion_Id'
+  public getCaste(religion_id: number) {
+    let url = 'http://192.168.0.127:8082/api/caste/getCaste/' + religion_id;
     return this.http.get(url);
   }
- public getSubcaste(caste_id:number)
- {
-  let url='http://192.168.0.127:8082/api/caste/getSubcaste/${caste_id}'
-  return this.http.get(url);
+  public getSubcaste(caste_id: number) {
+    let url = 'http://192.168.0.127:8082/api/subcaste/getSubcaste/' + caste_id;
+    return this.http.get(url);
 
- }
   }
+}
 //   apiBaseUrl='http://locahost/dev/tcxapp';
 
 // public getUsers()
