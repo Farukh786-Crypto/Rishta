f09e2e7f0c1040642b8f62304afeff173d79db57
Dadasaheb_Bharat <59571843+DadasahebBharat@users.noreply.github.com>
2020-02-26 11:47:11 +0530

    commited by raj

--- src/app/layout/admin/dashboard/grooms/grooms.component.ts
+++ src/app/layout/admin/dashboard/grooms/grooms.component.ts
@@ -1,11 +1,11 @@
 import { Router } from '@angular/router';
-import { MatDialog} from '@angular/material';
+import { MatDialog } from '@angular/material';
 import { Component, OnInit, ViewChild, AfterViewInit, Inject } from '@angular/core';
- import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { map } from 'rxjs/operators';
- import { Observable, of as observableOf, merge } from 'rxjs';
-import { MatTable  } from '@angular/material/table';
+import { Observable, of as observableOf, merge } from 'rxjs';
+import { MatTable } from '@angular/material/table';
 import { ViewComponent } from '../view/view.component';
 
 @Component({
@@ -14,32 +14,30 @@ import { ViewComponent } from '../view/view.component';
   styleUrls: ['./grooms.component.css'],
   providers: []
 })
-export class GroomsComponent implements AfterViewInit, OnInit{
+export class GroomsComponent implements AfterViewInit, OnInit {
 
-  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: false}) sort: MatSort;
-  @ViewChild(MatTable, {static: false}) table: MatTable<DataTableItem>;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatTable, { static: false }) table: MatTable<DataTableItem>;
   dataSource: GroomsComponent;
 
-  displayedColumns = ['id','photo', 'detail','action'];
+  displayedColumns = ['id', 'photo', 'detail', 'action'];
 
-  constructor( private route: Router,public dialog: MatDialog) { }
-  animal: string;
- 
+  constructor(private route: Router, public dialog: MatDialog) { }
 
   ngOnInit() {
- 
- this.dataSource = new GroomsComponent(this.route,this.dialog);
-   }
-   ngAfterViewInit() {
+
+    this.dataSource = new GroomsComponent(this.route, this.dialog);
+  }
+  ngAfterViewInit() {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
   }
-  
+
   data: DataTableItem[] = EXAMPLE_DATA;
   connect(): Observable<DataTableItem[]> {
-   const dataMutations = [
+    const dataMutations = [
       observableOf(this.data),
       this.paginator.page,
       this.sort.sortChange
@@ -49,7 +47,9 @@ export class GroomsComponent implements AfterViewInit, OnInit{
       return this.getPagedData(this.getSortedData([...this.data]));
     }));
   }
-  disconnect() {}
+
+  disconnect() { }
+
   private getPagedData(data: DataTableItem[]) {
     const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
     return data.splice(startIndex, this.paginator.pageSize);
@@ -62,54 +62,54 @@ export class GroomsComponent implements AfterViewInit, OnInit{
     return data.sort((a, b) => {
       const isAsc = this.sort.direction === 'asc';
       switch (this.sort.active) {
-      //   case 'name': return compare(a.name, b.name, isAsc);
+        //   case 'name': return compare(a.name, b.name, isAsc);
         case 'id': return compare(+a.id, +b.id, isAsc);
         default: return 0;
       }
     });
   }
-  
-  openDialog(): void {
-     const dialogRef = this.dialog.open(ViewComponent, {
-      width: '900px',height:'600px'
-      
+
+  openDialog(params): void {
+    console.log(params);
+    const dialogRef = this.dialog.open(ViewComponent, {
+      width: '900px', height: '600px',
+      data: params
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      this.animal = result;
     });
   }
-   detail: any
-
+  detail: any
 }
 export interface DataTableItem {
-  detail : {
-    name:string,
-    age:number;
-    religion:string;
-    mothertongue:string;
-    city:string;
-    maritalstatus:string;
+  detail: {
+    name: string,
+    age: number;
+    religion: string;
+    mothertongue: string;
+    city: string;
+    maritalstatus: string;
   };
   id: number;
-  photo:any;
+  photo: any;
 
-  
+
 }
- const EXAMPLE_DATA: DataTableItem[] = [ 
-    {id: 1,photo:'./assets/Images/1.jpg', detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi',city:'pune',maritalstatus:'married'}},
-    {id: 2,photo:'./assets/Images/2.jpg', detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi',city:'pune',maritalstatus:'married'}},      {id: 3,photo:'./assets/Images/h1.jpg', detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi',city:'pune',maritalstatus:'married'}},
-     {id: 4,photo:'./assets/Images/4.jpg', detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi',city:'pune',maritalstatus:'married'}},
-     {id: 5, photo:'./assets/Images/h2.jpg', detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi',city:'pune',maritalstatus:'married'}},
-     {id: 6, photo:'./assets/Images/5.jpg', detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi',city:'pune',maritalstatus:'married'}},
-     {id: 7, photo:'./assets/Images/h3.jpg', detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi',city:'pune',maritalstatus:'married'}},
-     {id: 8,photo:'./assets/Images/6.jpg',  detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi',city:'pune',maritalstatus:'married'}},
-     {id: 9,photo:'./assets/Images/h5.jpg',  detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi',city:'pune',maritalstatus:'married'}},
-     {id: 10,photo:'./assets/Images/h6.jpg',  detail:{name:'Sastura',age:26,religion:'hindu',mothertongue:'marathi ',city:'pune',maritalstatus:'married'}},
-     
-    
-  ];
+const EXAMPLE_DATA: DataTableItem[] = [
+  { id: 1, photo: './assets/Images/1.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi', city: 'pune', maritalstatus: 'married' } },
+  { id: 2, photo: './assets/Images/2.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi', city: 'pune', maritalstatus: 'married' } },
+  { id: 3, photo: './assets/Images/h1.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi', city: 'pune', maritalstatus: 'married' } },
+  { id: 4, photo: './assets/Images/4.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi', city: 'pune', maritalstatus: 'married' } },
+  { id: 5, photo: './assets/Images/h2.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi', city: 'pune', maritalstatus: 'married' } },
+  { id: 6, photo: './assets/Images/5.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi', city: 'pune', maritalstatus: 'married' } },
+  { id: 7, photo: './assets/Images/h3.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi', city: 'pune', maritalstatus: 'married' } },
+  { id: 8, photo: './assets/Images/6.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi', city: 'pune', maritalstatus: 'married' } },
+  { id: 9, photo: './assets/Images/h5.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi', city: 'pune', maritalstatus: 'married' } },
+  { id: 10, photo: './assets/Images/h6.jpg', detail: { name: 'Sastura', age: 26, religion: 'hindu', mothertongue: 'marathi ', city: 'pune', maritalstatus: 'married' } },
+
+
+];
 
 function compare(a, b, isAsc) {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
--- src/app/layout/admin/dashboard/view/view.component.ts
+++ src/app/layout/admin/dashboard/view/view.component.ts
@@ -1,27 +1,30 @@
-import { Component } from '@angular/core'
+import { Component, OnInit, Inject } from '@angular/core'
 import { ViewService } from './view.service'
+import { MatDialogRef } from '@angular/material';
 
-
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
-    selector : 'view-comp',
-    templateUrl : './view.component.html',
-    styleUrls : ['./view.component.css'],
-   providers : [ViewService]
+  selector: 'view-comp',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.css'],
+  providers: [ViewService]
 })
 
-export class ViewComponent
-{
-  
-    dialogRef: any;
+export class ViewComponent implements OnInit {
 
-    constructor()
-    {
-      
-    }
-     
-  
-    onNoClick(): void {
-      this.dialogRef.close();
-    }
-  
+  // dialogRef: any;
+
+  constructor(public dialogRef: MatDialogRef<ViewComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+    console.log(data);
+  }
+
+  ngOnInit() {
+
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
