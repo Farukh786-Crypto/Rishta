06a99958f7a6edea9604baea2435648247cf6377
sujatabpawar <sujatapawar07@gmail.com>
2020-02-27 13:34:00 +0530

    add

--- src/app/UserModule/CareerDetailsForm/career.component.ts
+++ src/app/UserModule/CareerDetailsForm/career.component.ts
@@ -13,73 +13,6 @@ import { CareerProfile } from 'src/app/model';
     providers: [CareerService]
 })
 export class CareerComponent implements OnInit {
-<<<<<<< HEAD
-
-    careerDetails: FormGroup
-    country: {};
-    state: {};
-    city: {};
-    bachlorDegree: {};
-    highestDegree: {};
-    emplyeed: {};
-    occupation: {};
-
-
-    CareerService: any;
-    constructor(private toastr: ToastrService, private Career: CareerService) { }
-     
-    
-
-  stateInfo: any[] = [];
-  countryInfo: any[] = [];
-  cityInfo: any[] = [];
-
-  ngOnInit() {
- this.getCountries();
- this.careerDetails = new FormGroup({
-    Country: new FormControl('', Validators.required),
-    State: new FormControl('', [Validators.required]),
-    City: new FormControl('', [Validators.required]),
-    HighestDegree: new FormControl('', [Validators.required]),
-    BachlorDegree: new FormControl('', [Validators.required]),
-    Emplyeed: new FormControl('', [Validators.required]),
-    Occupation: new FormControl('', [Validators.required]),
-    Income: new FormControl('', [Validators.required]),
-    Express: new FormControl('', [Validators.required])
-});
-
-
-
-  }
- 
-  getCountries(){
-    this.Career.allCountries().
-    subscribe(
-      data => {
-        this.countryInfo=data.Countries;
-        //console.log('Data:', this.countryInfo);
-      },
-      err => console.log(err),
-      () => console.log('complete')
-    )
-  }
-
-  onChangeCountry(countryValue) {
-      console.log("hi");
-    this.stateInfo=this.countryInfo[countryValue].States;
-    this.cityInfo=this.stateInfo[0].Cities;
-    // console.log(this.cityInfo);
-  }
-
-  onChangeState(stateValue) {
-    this.cityInfo=this.stateInfo[stateValue].Cities;
-    //console.log(this.cityInfo);
-  }
-    
-}
-
-     
-=======
     
     stateInfo: any[] = [];
     countryInfo: any[] = [];
@@ -129,4 +62,3 @@ careerDetails: FormGroup
         console.log(this.cityInfo);
     }
 }
->>>>>>> acfb08b4ac5cd7f67c1dd16946a7aa067f6e4f5d
