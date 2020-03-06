4472cfa9feab1cbee9771f498353db0a17fbe0bc
Farukh786-Crypto <60135145+Farukh786-Crypto@users.noreply.github.com>
2020-03-06 11:31:04 +0530

    commit

--- src/app/UserModule/Error.component.ts
+++ src/app/UserModule/Error.component.ts
@@ -1,22 +1,8 @@
 import { Component } from '@angular/core'
-
 @Component({
 selector:'Error-comp',
 templateUrl:'./Error.component.html'
-
-    
-
 })
-
-
 export class ErrorComponent {
-
     title: string = 'Error Component is Occured';
-
 }
-
-
-
-
-
-
