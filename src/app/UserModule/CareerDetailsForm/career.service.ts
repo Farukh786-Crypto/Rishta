c1017dd6c7276636732c1a957b46d8cf5bc7bc60
Farukh786-Crypto <60135145+Farukh786-Crypto@users.noreply.github.com>
2020-03-06 10:58:08 +0530

    Committed by FRK 10:57 AM

--- src/app/UserModule/CareerDetailsForm/career.service.ts
+++ src/app/UserModule/CareerDetailsForm/career.service.ts
@@ -6,13 +6,11 @@ import { CareerProfile } from 'src/app/model';
 export class CareerService {
   constructor(private http: HttpClient) { }
   // url: string = "https://raw.githubusercontent.com/sagarshirbhate/Country-State-City-Database/master/Contries.json";
-  url: string ="http://192.168.0.122:8080/api/countries/countries";
+   url: string ="http://192.168.0.122:8080/api/countries/countries";
   //   private url='http://192.168.0.127:8082/api/addcarrierdetails'
   //   addContent(content:CareerProfile):Observable<CareerProfile>{
-
   //     const httpOption = {headers:new HttpHeaders({'Content-Type':'application/json'})};
   //     return this.http.post<CareerProfile>(this.url+'/create',content,httpOption);
-
   // }    
   //url: string="http://192.168.0.122:8080/api/getdetails/Carrier"   
   allCountries(): Observable<any> {
