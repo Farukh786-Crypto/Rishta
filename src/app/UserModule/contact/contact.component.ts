eeb22b9d5cf774336dd357a012847f8eaedab6d0
sujatabpawar <sujatapawar07@gmail.com>
2020-03-07 19:44:26 +0530

    add

--- src/app/UserModule/contact/contact.component.ts
+++ src/app/UserModule/contact/contact.component.ts
@@ -5,6 +5,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  state=['Pune','Nasik','Mumbai','Nagpur']
+  contact=['7744332211',,'8252523252','8595982255','9876543210'    
+  ]
+  Address=[':Flat n0o. 101, Kranti chowk,pune',
+  ':Flat n0o. 101, Kranti chowk,pune',
+  ':Flat n0o. 101, Kranti chowk,pune',
+  ':Flat n0o. 101, Kranti chowk,pune'
+
+  ]
+  contactperson=['Mahesh','Jayesh','Rituraj','Rakesh']
+
+details=[{name:'Mahesh',state:'Pune',contact:'7744332211',address:'Flat n0o. 101, Kranti chowk,pune'},
+{name:'Jayesh',state:'Nashik',contact:'8285675634',address:'Flat n0o. 102, Kranti chowk,pune'},
+{name:'Rakesh',state:'Nashik',contact:'8285675634',address:'Flat n0o. 102, Kranti chowk,pune'}]
+
+
+
   constructor() { }
   ngOnInit() {
   }
